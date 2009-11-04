609a9e14d7a91aa420360c8107714b6c6da7536f
Paul <paul@wambam.lan>
2009-11-04 21:51:45 +0000

    Commented out the salting feature in create_user() function.

--- dwardle/models/user_model.php
+++ dwardle/models/user_model.php
@@ -5,7 +5,7 @@ class user_model extends Model
 	function verify()
 	{
 		$this->db->where('username', $this->input->post('username'));
-		$this->db->where('password', substr(sha1($this->input->post('password')), 0, -7));
+		$this->db->where('password', sha1($this->input->post('password')));
 		$get = $this->db->get('users');
 		
 		if($get->num_rows == 1)
@@ -16,17 +16,20 @@ class user_model extends Model
 	
 	function create_user()
 	{
-		$salt = mt_rand(5, 15);
+		// $salt = mt_rand(5, 15);
 		$user_data = array(
 				'username'	=>	$this->input->post('username'),
 				'email'		=>	$this->input->post('email'),
-				'password'	=>	sha1($this->input->post('password')) . $salt,
-				
+				'password'	=>	sha1($this->input->post('password')),
 				'first_name'	=>	$this->input->post('first_name'),
-				'last_name'		=>	$this->input->post('last_name')
+				'last_name'		=>	$this->input->post('last_name'),
+				'registered'	=>  date("Y-m-d h:i:s")
 		);
 		
 		$send = $this->db->insert('users', $user_data);
+		$this->load->library('firephp');
+		
+		$this->firephp->log($send);
 		return $send;
 	}
 }
